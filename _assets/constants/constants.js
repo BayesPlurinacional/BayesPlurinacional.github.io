3f5b25b1348f86edf346d33af56a7477fc763b2e
TheChieft <aandresfelipeyanezz@gmail.com>
2025-01-30 15:12:47 -0500

    Diseño mejorado de navbar

---
+++ _assets/constants/constants.js
@@ -0,0 +1,13 @@
+const SOCIAL_MEDIA = {
+  email: "bayesplurinacional@gmail.com",
+  twitter: "https://twitter.com/BayesPlural",
+  instagram: "https://instagram.com/bayesplurinacional",
+  linkedin: "https://www.linkedin.com/company/bayes-plurinacional/",
+  youtube: "https://www.youtube.com/@bayesdelsur"
+};
+
+const FOOTER_LINKS = {
+  somos: "/_pages/es/somos.html",
+  galeria: "/_pages/es/galeria.html",
+  contacto: "/_pages/es/contacto.html",
+};
--- _components/es/footer.js
+++ _components/es/footer.js
@@ -1,51 +1,58 @@
 class CustomFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-          <footer class="footer bg-dark text-white text-center py-3">
-              <div class="container">
-                  <div class="row">
-                      <div class="col-md-6">
-                          <h5>Bayes Plurinacional</h5>
-                          <p>Evento de estadística y ciencia de datos en América Latina. Conectamos a la comunidad para compartir conocimiento y experiencias.</p>
-                      </div>
-                      <div class="col-md-3">
-                          <h5>Enlaces</h5>
-                          <ul class="list-unstyled">
-                              <li><a href="/_pages/es/somos.html">Somos</a></li>
-                              <li><a href="/_pages/es/galeria.html">Galería</a></li>
-                              <li><a href="/_pages/es/contacto.html">Contacto</a></li>
-                          </ul>
-                      </div>
-                      <div class="col-md-3">
-                          <h5>Síguenos</h5>
-                          <a href="#" class="social-icon"><i class="fab fa-facebook"></i></a>
-                          <a href="#" class="social-icon"><i class="fab fa-twitter"></i></a>
-                          <a href="#" class="social-icon"><i class="fab fa-instagram"></i></a>
-                      </div>
-                  </div>
-              </div>
-              <div class="footer-bottom bg-secondary py-2">
-                  © 2025 Bayes Plurinacional. Todos los derechos reservados.
-              </div>
-          </footer>
+      <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <!-- Sección 1: Sobre Nosotros -->
+                <div class="col-lg-3 col-md-6">
+                    <h5>Sobre Nosotros</h5>
+                    <ul>
+                        <li><a href="/_pages/es/somos.html">Quiénes Somos</a></li>
+                        <li><a href="/_pages/es/plural.html">Plural</a></li>
+                        <li><a href="/_pages/es/organizacion.html">Organización</a></li>
+                    </ul>
+                </div>
 
-          <style>
-          /* Navbar */
-.navbar {
-    background-color: var(--color-primario);
-    color: white;
-}
+                <!-- Sección 2: Congreso -->
+                <div class="col-lg-3 col-md-6">
+                    <h5>Congreso</h5>
+                    <ul>
+                        <li><a href="/_pages/es/cronograma.html">Cronograma</a></li>
+                        <li><a href="/_pages/es/disertantes.html">Disertantes</a></li>
+                        <li><a href="/_pages/es/seminarios.html">Seminarios Virtuales</a></li>
+                    </ul>
+                </div>
 
-.navbar .nav-link {
-    color: white;
-}
+                <!-- Sección 3: Galería -->
+                <div class="col-lg-3 col-md-6">
+                    <h5>Galería</h5>
+                    <ul>
+                        <li><a href="/_pages/es/galeria/2025.html">Fotos 2025</a></li>
+                        <li><a href="/_pages/es/galeria/2024.html">Fotos 2024</a></li>
+                        <li><a href="/_pages/es/galeria/2023.html">Fotos 2023</a></li>
+                    </ul>
+                </div>
 
-.navbar .nav-link:hover {
-    color: var(--color-secundario);
-}
+                <!-- Sección 4: Redes Sociales -->
+                <div class="col-lg-3 col-md-6">
+                    <h5>Síguenos</h5>
+                    <div class="social-icons">
+                        <a href="https://twitter.com/BayesPlural" target="_blank"><i class="fab fa-twitter"></i></a>
+                        <a href="https://instagram.com/bayesplurinacional" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.linkedin.com/company/bayes-plurinacional/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        <a href="mailto:bayesplurinacional@gmail.com"><i class="fas fa-envelope"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-</style>
-      `;
+        <p class="footer-bottom">
+            © 2025 Bayes Plurinacional. Todos los derechos reservados.
+        </p>
+      </footer>
+    `;
   }
 }
+
 customElements.define("custom-footer", CustomFooter);
